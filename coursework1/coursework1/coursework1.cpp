d2c01bd83e0d2c93634a1b77c70d869e122cf04c
Christopher <christopher.cometto@hotmail.it>
2019-10-17 15:32:54 +0100

    First solution related to avoid configuration repetitions

--- coursework1/coursework1/coursework1.cpp
+++ coursework1/coursework1/coursework1.cpp
@@ -1,60 +1,86 @@
 #include <iostream>
+#include <stack>
 #include "Configuration.h"
 #include "Graph.h"
+
+using namespace std;
 
 int boardSize = 3;
 int i = 0;
 
-int makePermutations(Graph* graph, Configuration conf)
-{
-	if (!conf.isFinal()) {
-		int* moves = conf.getAvailableMoves();
-		int count = 0;
-		int voidTile = conf.getVoidTile();
-		for (int i = 0; i < 4; i++)
-		{
-			if (moves[i] != -1) {
-				Arch* a = graph->getArch(moves[i], voidTile, conf.getTable()[moves[i]]);
-				if (a != nullptr && graph->confExist(conf.getTable(), *a)) {
+struct Move {
+	int coord;
+	int voidTile;
+	vector<int> conf;
+};
 
-				}
-				else {
-					graph->addConf(moves[i], voidTile, conf.getTable()[moves[i]], conf.getTable());
-					conf.swapTiles(voidTile, moves[i]);
-					count = makePermutations(graph, conf);
+bool confExist(vector<int> conf, vector<vector<int>>& confs) {
+	for (vector<int> c : confs) {
+		if (c == conf) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool removeConf(vector<int> conf, vector<vector<int>>& confs) {
+	for (rsize_t i = 0; i < confs.size(); i++) {
+		if (confs[i] == conf) {
+			confs.erase(confs.begin() + i);
+		}
+	}
+	return false;
+}
+
+void test(Configuration& conf) {
+	stack<Move> remMoves;
+	vector<vector<int>> confs;
+	Configuration* c;
+
+	int voidTile = conf.getVoidTile();
+	int* moves = conf.getAvailableMoves();
+
+	for (int i = 0; i < 4; i++) {
+		if (moves[i] != -1) {
+			Move m = { moves[i], voidTile, conf.getTable() };
+			remMoves.push(m);
+		}
+	}
+
+	while (!remMoves.empty()) {
+		Move m = remMoves.top();
+		remMoves.pop();
+
+		c = new Configuration(m.conf, boardSize);
+		c->swapTiles(m.voidTile, m.coord);
+
+		if (c->isFinal()) {
+			// Make something to count the data
+
+			c->print();
+			cout << "\n" << i << " final\n\n";
+			i++;
+		}
+		else if (confExist(c->getTable(), confs)) {
+			removeConf(c->getTable(), confs);
+		} else {
+			confs.push_back(c->getTable());
+			int* moves = c->getAvailableMoves();
+			int voidTile = c->getVoidTile();
+
+			for (int i = 0; i < 4; i++) {
+				if (moves[i] != -1) {
+					Move m = { moves[i], voidTile, c->getTable() };
+					remMoves.push(m);
 				}
 			}
-			
 		}
-		return count;
+		delete c;
 	}
-	//conf.print();
-	cout << "is final " << i <<" \n";
-	i++;
-	return 1;
 }
-
-int test(Configuration conf)
-{
-	int* moves = conf.getAvailableMoves();
-	int count = 0;
-	int voidTile = conf.getVoidTile();
-	vector<Arch> arches;
-	Graph* graph = new Graph(arches, boardSize * boardSize);
-	for (int i = 0; i < 4; i++)
-	{
-		if (moves[i] != -1) {
-			graph->addConf(moves[i], voidTile, conf.getTable()[moves[i]], conf.getTable());
-			conf.swapTiles(voidTile, moves[i]);
-			count += makePermutations(graph, conf);
-		}
-	}
-	return count;
-}
-
 
 int main()
 {
 	Configuration* conf = new Configuration(boardSize);
-	std::cout << test(*conf);
+	test(*conf);
 }
